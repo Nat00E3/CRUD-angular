4b9e328e069149cafb337f5556dc39f9ae615aef
Nat00E3 <njordao@findes.org.br>
2024-04-05 17:41:47 -0300

    criação da página

---
+++ front/src/app/carros/carros.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CarrosRoutingModule } from './carros-routing.module';
+import { CarrosComponent } from './carros/carros.component';
+
+
+@NgModule({
+  declarations: [
+    CarrosComponent
+  ],
+  imports: [
+    CommonModule,
+    CarrosRoutingModule
+  ]
+})
+export class CarrosModule { }
